88ba966ed3641423f94d79e8f01a55405c5630a6
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2025-02-16 14:05:42 +0300

    задание выполнено

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,65 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+document.addEventListener("DOMContentLoaded", (e) => {
+	//<>=================================
+	// const tabHeaderItemO = document.querySelector('.tabheader__items');
+	// const tabContentO = document.querySelector('.tabcontainer');
+	// tabHeaderItemO.insertAdjacentHTML('beforeend', '<div class="tabheader__item">Постное</div>');
+	// tabContentO.insertAdjacentHTML('afterbegin', `<div class="tabcontent hide">
+	// 						<img src="img/tabs/post.jpg" alt="post">
+	// 						<div class="tabcontent__descr">
+	// 							Наше специальное “Постное меню” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения. Полная гармония с собой и природой в каждом элементе! Все будет Ом!
+	// 						</div>
+	// 					</div>`);
+	//</>=================================
+	const tabContents = document.querySelectorAll('.tabcontent');
+	const tabHeaderItems = document.querySelectorAll('.tabheader__item');
+	const tabHeaderItem = document.querySelector('.tabheader__items');
 
+	// скрыть контент и заголовок таба 
+	function hideTabs() {
+		tabContents.forEach(el => {
+			el.classList.add('hide');
+			el.classList.remove('show', 'fade');
+		});
+		tabHeaderItems.forEach(el => {
+			el.classList.remove('tabheader__item_active');
+		});
+	}
+
+	// показать контент и акивный заголовок таба 
+	function showTabs(i = 0) {
+		tabContents[i].classList.add('show', 'fade');
+		tabContents[i].classList.remove('hide');
+		tabHeaderItems[i].classList.add('tabheader__item_active');
+	}
+
+	// мой вариант
+	tabHeaderItem.addEventListener("click", (e) => {
+		const et = e.target;
+		tabHeaderItems.forEach((el, i) => {
+			if (el == et) {
+				hideTabs();
+				showTabs(i);
+			}
+		});
+	});
+
+	// вариант из урока
+	// tabHeaderItem.addEventListener("click", (e) => {
+	// 	const et = e.target;
+	// 	if (et && et.classList.contains('tabheader__item')) {
+	// 		tabHeaderItems.forEach((el, i) => {
+	// 			if (et == el) {
+	// 				hideTabs();
+	// 				showTabs(i);
+	// 			}
+	// 		});
+	// 	}
+	// });
+
+	// первоначально сброс состояний
+	hideTabs();
+	showTabs();
+});
